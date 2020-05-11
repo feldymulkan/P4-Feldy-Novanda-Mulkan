aab10f497ce2bab886ca32694a11862e28025924
FeldyNovandaMulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2020-05-11 19:15:15 +0800

    Create Jobsheet-1-C++.cpp
    Membuat program array satu dimensi yang menampilkan NIM masing-masing mahasiswa dengan C++

---
+++ Jobsheet-1-C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+main()
+{
+char nim[5]={"F1B019037","F1B019
+055",
+"F1B019007","F1B019021","F1B019028"};
+	
+	cout<<(nim[0])<<"\n";
+	cout<<(nim[1])<<"\n";
+	cout<<(nim[2])<<"\n";
+	cout<<(nim[3])<<"\n";
+}
