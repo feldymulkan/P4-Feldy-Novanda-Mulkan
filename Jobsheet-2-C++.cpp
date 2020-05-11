94d49745dfa7cb643fbd4484f79e67bb069ba000
FeldyNovandaMulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2020-05-11 19:21:41 +0800

    Create Jobsheet-2-C++.cpp
    Membuat program mengisi dan menampilkanelemen array dengan index array sesuai NIM terakhir  setiap mahasiswa dengan bahasa C++

---
+++ Jobsheet-2-C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	
+	int nim[5];
+	cout<<"Nama : Feldy Novanda Mulkan \n";
+	cout<<"Akhiran NIM = 5\n";
+	cout<<"Masukkan Nilai 1 : ";cin>>nim[0];
+	cout<<"Masukkan Nilai 2 : ";cin>>nim[1];
+	cout<<"Masukkan Nilai 3 : ";cin>>nim[2];
+	cout<<"Masukkan Nilai 4 : ";cin>>nim[3];
+	cout<<"Masukkan Nilai 5 : ";cin>>nim[4];
+	cout<<"\n----------------------------\n";
+	cout<<"Nilai 1 : "<<nim[0]<<"\n";
+	cout<<"Nilai 2 : "<<nim[1]<<"\n";
+	cout<<"Nilai 3 : "<<nim[2]<<"\n";
+	cout<<"Nilai 4 : "<<nim[3]<<"\n";
+	cout<<"Nilai 5 : "<<nim[4]<<"\n";
+}
