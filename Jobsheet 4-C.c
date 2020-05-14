6d43033871498547a379881a95c134729ed228fd
FeldyNovandaMulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2020-05-14 10:20:40 +0800

    Create Jobsheet 4-C.c
    Membuat program Pengurangan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa

---
+++ Jobsheet 4-C.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+main()
+{
+int matrix1[2][2],
+matrix2[2][2],i,j;
+printf("______________________________\n");
+printf("|Program Pengurangan Mattriks |\n");
+printf("______________________________\n");
+
+printf("NIM 1 : F1B019055 \n");
+printf("NIM 2 : F1B019037 \n");
+printf("Kelompok 7 \n");
+printf("Data Matriks A \n");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){ 
+			printf("Elemen[%d][%d]: ",i,j); 
+			scanf("%d",&matrix1[i][j]);
+		}
+		}
+printf("\nMATRIX2\n");
+	for(i=0;i<2;i++){ 
+		for (j=0;j<2;j++){ 
+			printf("Elemen[%d][%d]: ",i,j); 
+			scanf("%d",&matrix2[i][j]);
+		}
+		}
+printf("\nHasil Pengurangan matriks\n\n");
+	for(i=0;i<2;i++){
+		for( j=0;j<2;j++)
+			printf("%d\t",matrix1[i][j]-matrix2[i][j]); 
+			printf("\n");
+	}
+}
