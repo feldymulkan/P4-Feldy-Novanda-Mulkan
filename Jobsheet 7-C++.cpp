f3c453419dd928cbe51cdc2c1f7d48c4ac0adf6b
FeldyNovandaMulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2020-05-14 10:26:12 +0800

    Create Jobsheet 7-C++.cpp
    Membuat program Mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap siswa.

---
+++ Jobsheet 7-C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"Akhiran NIM : 5\n";
+	int x[4],i;
+	for(i=1;i<=5;i++)
+	{
+	cout<<"Masukkan Nim "<<i<<" : ";
+	cin>>x[i];
+	}
+	return 0;
+}
